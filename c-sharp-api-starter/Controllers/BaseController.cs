6340c035367af28ed0397136c1967f08176308d2
yllgashi <yllxgashi@gmail.com>
2023-01-10 04:22:48 +0100

    fetch userId from jwt in base controller

--- c-sharp-api-starter/Controllers/BaseController.cs
+++ c-sharp-api-starter/Controllers/BaseController.cs
@@ -4,5 +4,9 @@ namespace c_sharp_api_starter.Controllers
 {
     public class BaseController : ControllerBase
     {
+        public string UserId
+        {
+            get { return User.Claims.Where(c => c.Type == "UserId").Select(x => x.Value).FirstOrDefault(); }
+        }
     }
 }
